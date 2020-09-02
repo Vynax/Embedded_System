2f0fb64f1bc1b9a04054b0ce99782a695a79066a
Vynax <22928806+Vynax@users.noreply.github.com>
2020-09-02 21:37:49 +0800

    刪除空格及換行字元
    拿掉空格及換行字元就可以直接比較了

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import sys
 
 text1 = open("my_shopping_list.txt").readlines()
 text2 = open("friends_shopping_list.txt").readlines()
-
-file1 = open("my_shopping_list.txt").read()
-file2 = open("friends_shopping_list.txt").read()
+fileX = ["", ""]
+fileY = ["", ""]
+fileX[0] = open("my_shopping_list.txt").read()
+fileX[1] = open("friends_shopping_list.txt").read()
 
 delta = difflib.context_diff(
     text1, text2, "my_shopping_list.txt", "friends_shopping_list.txt")
@@ -20,10 +21,16 @@ print("")
 #alpha = list(d.compare(file1,file2))
 sys.stdout.writelines(alpha)
 
-k = difflib.SequenceMatcher(None, file1, file2)
+k = difflib.SequenceMatcher(None, fileX[0], fileX[1])
 print("")
 print(k.ratio())
 
-m = difflib.SequenceMatcher(lambda x: x ==" ", file1, file2)
+for x in range(2):
+    fileY[x] = fileX[x].replace(" ", "").replace("\n", "")
+    #fileY[x] = fileY[x].replace("\n", "")
+
+
+#fileY[1] = fileX[1].replace(" ", "")
+m = difflib.SequenceMatcher(lambda x: x ==" ", fileY[0], fileY[1])
 print("")
 print(m.ratio())
